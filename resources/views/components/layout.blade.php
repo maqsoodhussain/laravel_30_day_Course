30fbf1b859dba3a9b9e2eb6512085c87ebabdefd
maqsoodhussain <maqsoodhussain@gdcbeerwah.edu.in>
2025-09-16 15:56:44 +0530

    Day 3 - components and layouts

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -15,7 +15,7 @@
             <a href="/">Home</a>
             <a href="/about">About</a>
             <a href="/contact">Contact</a>
-            
+        <nav>
         <!-- <?php echo $slot ?> -->
          {{ $slot }}
     </body>
